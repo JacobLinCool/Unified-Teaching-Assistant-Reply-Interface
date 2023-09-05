fb127b9791be4fe83167d5c1cdf0513b06f98afe
JacobLinCool <jacoblincool@gmail.com>
2023-09-06 05:19:13 +0800

    test: remove openai test

--- packages/openai-assistant/tests/pkg.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, it } from "vitest";
-import _package from "../package.json";
-import { pkg } from "../src";
-
-it("version", () => {
-	expect(pkg.version).toBe(_package.version);
-});
